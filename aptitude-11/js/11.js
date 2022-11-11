efc87f252e00ad41a2477a8b7c85b6963f177489
monisha023 <monishachezhiyan@gmail.com>
2022-11-11 18:25:09 +0530

    Aptitude program

---
+++ aptitude-11/js/11.js
@@ -0,0 +1,5 @@
+var ds= parseInt(prompt("Enter a speed of ds:"));
+var us= parseInt(prompt("Enter a speed of us:"));
+var speedStream;
+speedStream = 1/2 *(ds+us);
+alert(speedStream);
---
+++ aptitude-12/js/12.js
@@ -0,0 +1,6 @@
+var p=parseInt(prompt("Enter a amount:"));
+var r=parseInt(prompt("Enter a interest"));
+var t=parseInt(prompt("Enter a years:"));
+var sI;
+sI = (p*r*t)/100;
+alert(sI);

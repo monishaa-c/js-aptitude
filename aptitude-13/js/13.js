b5d4a90b7d9a9a5598de2371f42db1e7fac50320
monisha023 <monishachezhiyan@gmail.com>
2022-11-12 17:54:02 +0530

    Aptitude program

---
+++ aptitude-13/js/13.js
@@ -0,0 +1,4 @@
+var d = parseInt(prompt("Enter a diagonal value:"));
+var square;
+square = 1/2*(d*d);
+alert(square);
---
+++ aptitude-14/js/14.js
@@ -0,0 +1,7 @@
+var value = parseInt(prompt("Enter a price of 20 dozen toy: "));
+var cp = value/12; // cp of one toy
+var profit, sp, pp;
+var sp = parseInt(prompt("Enter a SP of one toy :"));
+profit = sp - cp;
+pp = (profit/cp)*100;
+alert(pp);

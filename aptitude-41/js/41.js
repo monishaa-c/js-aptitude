c74753ab9d7a8fd3be84ec93c888e3531d53f029
monisha023 <monishachezhiyan@gmail.com>
2022-11-22 14:45:19 +0530

    Aptitude program

--- aptitude-41/js/41.js
+++ aptitude-41/js/41.js
@@ -1,4 +1,17 @@
-var r = parseInt(prompt("Enter a radius value:"));
-var h = parseInt(prompt("Enter a height value:"));
-var surface = 2*22/7*r*(h+r);
-alert(surface);
+//var r = parseInt(prompt("Enter a radius value:"));
+//var h = parseInt(prompt("Enter a height value:"));
+//var surface = 2*22/7*r*(h+r);
+//alert(surface);
+  
+
+//var a,b,c,d;
+//a= 10;
+//b=15;
+//c=12;
+//d=(a+b),(a+c);
+//alert(d);
+
+var  a = "10";
+var b = "apple";
+var c= a+b;
+alert(c);
